c535187f5cf4007fff4e564ec5ab5a597411f0d6
luismagr <desarrollo@connect2tic.es>
2014-02-06 14:09:01 +0100

    Issue #36. Añadida entidad Speciality

---
+++ src/Dentoleti/PersonalBundle/Entity/Speciality.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Dentoleti\PersonalBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Speciality
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Speciality
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Speciality
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
