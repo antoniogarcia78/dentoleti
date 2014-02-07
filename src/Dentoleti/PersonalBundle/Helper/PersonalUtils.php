617c964b296775cf35065482d50b09178d01a897
luismagr <desarrollo@connect2tic.es>
2014-02-07 18:00:46 +0100

    Issue #48. Helper olvidado del commit anterior

---
+++ src/Dentoleti/PersonalBundle/Helper/PersonalUtils.php
@@ -0,0 +1,28 @@
+<?php
+namespace Dentoleti\PersonalBundle\Helper;
+
+/**
+ * Helper class for Personal
+ */
+class PersonalUtils
+{
+
+	/**
+	 * This method set the $personal to null values
+	 */
+	public function setNullPersonal($personal)
+	{
+		$personal->setName(null);
+		$personal->setPosition(null);
+		$personal->setAddress(null);
+		$personal->setPostalCode(null);
+		$personal->setTown(null);
+		$personal->setPhone1(null);
+		$personal->setPhone2(null);
+		$personal->setObservations(null);
+		$personal->setRegistrationDate(null);
+		$personal->setActive(null);
+
+		return $personal;
+	}
+}
