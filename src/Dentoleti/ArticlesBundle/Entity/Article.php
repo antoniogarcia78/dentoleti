75e517aff8cc863b253e8e9811478bf88679d062
luismagr <desarrollo@connect2tic.es>
2014-02-10 12:19:29 +0100

    Issue #82. Entidad Article creada

---
+++ src/Dentoleti/ArticlesBundle/Entity/Article.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace Dentoleti\ArticlesBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Article
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=100)
+     */
+    private $description;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="float")
+     */
+    private $price;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="vat", type="float")
+     */
+    private $vat;
+
+    /**
+     * @var string
+     *
+     * @ORM\OneToOne(targetEntity="Family")
+     * @ORM\JoinColumn(name="family_id", referencedColumnName="id", nullable=true)
+     */
+    private $family;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="registrationDate", type="datetime")
+     */
+    private $registrationDate;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Article
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     * @return Article
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set vat
+     *
+     * @param float $vat
+     * @return Article
+     */
+    public function setVat($vat)
+    {
+        $this->vat = $vat;
+
+        return $this;
+    }
+
+    /**
+     * Get vat
+     *
+     * @return float 
+     */
+    public function getVat()
+    {
+        return $this->vat;
+    }
+
+    /**
+     * Set family
+     *
+     * @param string $family
+     * @return Article
+     */
+    public function setFamily($family)
+    {
+        $this->family = $family;
+
+        return $this;
+    }
+
+    /**
+     * Get family
+     *
+     * @return string 
+     */
+    public function getFamily()
+    {
+        return $this->family;
+    }
+
+    /**
+     * Set registrationDate
+     *
+     * @param \DateTime $registrationDate
+     * @return Article
+     */
+    public function setRegistrationDate($registrationDate)
+    {
+        $this->registrationDate = $registrationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get registrationDate
+     *
+     * @return \DateTime 
+     */
+    public function getRegistrationDate()
+    {
+        return $this->registrationDate;
+    }
+}
