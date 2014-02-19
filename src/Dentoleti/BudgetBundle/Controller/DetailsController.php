7b28ea496d2d13e4a34ddd92895d3c5bd4bf8970
luismagr <desarrollo@connect2tic.es>
2014-02-19 14:19:02 +0100

    Issue # 103 #105. Añadido formulario y relación

--- src/Dentoleti/BudgetBundle/Controller/DetailsController.php
+++ src/Dentoleti/BudgetBundle/Controller/DetailsController.php
@@ -2,10 +2,14 @@
 namespace Dentoleti\BudgetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-//use Symfony\Component\HttpFoundation\Request;
+use Dentoleti\BudgetBundle\Entity\BudgetDetail;
+use Dentoleti\BudgetBundle\Form\BudgetDetail\BudgetDetailType;
 
 class DetailsController extends Controller
 {
+	/**
+     * Add a new budget detail in the system
+     */
 	public function addAction($budgetId)
 	{
 		$em = $this->getDoctrine()->getManager();
@@ -13,11 +17,63 @@ class DetailsController extends Controller
 		$budget = $em->getRepository('DentoletiBudgetBundle:Budget')
 			->findOneById($budgetId);
 
-		$budgetDetailsList = $em->getRepository('DentoletiBudgetBundle:BudgetDetail')
+		$petition = $this->getRequest();
+
+		$budgetDetail = new BudgetDetail();
+		$budgetDetail->setBudget($budget);
+
+		$form = $this->createForm(new BudgetDetailType(), $budgetDetail);
+
+		$form->handleRequest($petition);
+
+		if ($form->isValid()){
+			//save the form
+    		$em = $this->getDoctrine()->getManager();
+    		
+    		$em->persist($budgetDetail);
+    		$em->flush();
+
+            $this->get('session')->getFlashBag()->add(
+              'notice',
+              'El presupuesto se ha guardado correctamente'
+            );
+
+            $nextAction = $form->get('addItem')->isClicked()
+              ? 'budget_details_add'
+              : 'budget_details_list';
+
+            return $this->redirect($this->generateUrl($nextAction, array(
+                    'budgetId' => $budget->getId())));
+		}
+
+		//$budgetDetailsList = $em->getRepository('DentoletiBudgetBundle:BudgetDetail')
+		//	->findArticlesOfBudget($budget);
+		
+		//return $this->render('DentoletiBudgetBundle:Details:list.html.twig', array(
+        //	'budgetDetailsList' => $budgetDetailsList
+        //));
+		return $this->render('DentoletiBudgetBundle:Details:budget_detail.html.twig', array(
+        	'form' => $form->createView(),
+        	'budget' => $budget
+        ));
+	}
+
+	/**
+     * List all the budget details in the system
+     */
+    public function listAction($budgetId)
+    {
+    	$em = $this->getDoctrine()->getManager();
+
+    	$budget = $em->getRepository('DentoletiBudgetBundle:Budget')
+			->findOneById($budgetId);
+
+    	$budgetDetailsList = $em->getRepository('DentoletiBudgetBundle:BudgetDetail')
 			->findArticlesOfBudget($budget);
 		
 		return $this->render('DentoletiBudgetBundle:Details:list.html.twig', array(
         	'budgetDetailsList' => $budgetDetailsList
-      ));
-	}
+        ));
+    }
+
 }
