4affb5bc067db75e1320e8f8697f0b33e7ff5fb3
luismagr <desarrollo@connect2tic.es>
2014-02-23 08:32:33 +0100

    Issue #117 #118. Añadir tratamiento

--- src/Dentoleti/TreatmentBundle/Controller/DefaultController.php
+++ src/Dentoleti/TreatmentBundle/Controller/DefaultController.php
@@ -3,11 +3,52 @@
 namespace Dentoleti\TreatmentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Dentoleti\TreatmentBundle\Entity\Treatment;
+use Dentoleti\TreatmentBundle\Form\Treatment\TreatmentType;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * Add a new treatment in the system
+     */
+    public function addAction()
     {
-        return $this->render('DentoletiTreatmentBundle:Default:index.html.twig', array('name' => $name));
+        $petition = $this->getRequest();
+
+    	$treatment = new Treatment();
+		
+		$form = $this->createForm(new TreatmentType(), $treatment);
+		
+		$treatment->setTreatmentDate(new \DateTime());
+
+		$form->handleRequest($petition);
+
+		if ($form->isValid()){
+            //save the form
+    		$em = $this->getDoctrine()->getManager();
+    		
+    		$em->persist($treatment);
+    		$em->flush();
+
+            $this->get('session')->getFlashBag()->add(
+              'notice',
+              'El presupuesto se ha guardado correctamente'
+            );
+
+            $nextAction = $form->get('save')->isClicked()
+              ? 'treatment_add' //TODO cambiar por treatment_details_add
+              : 'treatment_add';
+
+            if ('budget_details_add' == $nextAction){
+                return $this->redirect($this->generateUrl($nextAction, array(
+                    'budgetId' => $treatment->getId())));
+            }
+
+            return $this->redirect($this->generateUrl($nextAction));
+     	}
+
+        return $this->render('DentoletiTreatmentBundle:Default:treatment.html.twig', array(
+        	'form' => $form->createView()
+        ));
     }
 }
