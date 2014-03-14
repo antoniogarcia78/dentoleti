20182ae363758c49af69edacf9fb38b6027aff95
luismagr <desarrollo@connect2tic.es>
2014-03-14 17:35:20 +0100

    Issue #133. Bundle creado

---
+++ src/Dentoleti/AccountingBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dentoleti\AccountingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('DentoletiAccountingBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
