80d3209f2d9dee4ab5d70fbb377444b17675196b
luismagr <desarrollo@connect2tic.es>
2014-03-16 12:23:22 +0100

    Issues #131 #135 #136. Añadidas las distintas funcionalidades

---
+++ src/Dentoleti/AccountingBundle/Form/PostingLines/PostingLineType.php
@@ -0,0 +1,40 @@
+<?php
+namespace Dentoleti\AccountingBundle\Form\PostingLines;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PostingLineType extends AbstractType
+{
+	/**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('amount', 'text', array(
+            'required' => false
+        ));
+        
+        $builder->add('add', 'submit');
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Dentoleti\AccountingBundle\Entity\PostingLine'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'dentoleti_accounting_posting_line';
+    }
+}
