04b9b9493523cacd15e7688a625d7df48124141e
luismagr <desarrollo@connect2tic.es>
2014-03-31 17:31:38 +0200

    Issue #161. Añadido método de pago

---
+++ src/Dentoleti/AccountingBundle/Entity/PaymentMethod.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Dentoleti\AccountingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PaymentMethod
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class PaymentMethod
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="methodName", type="string", length=20)
+     */
+    private $methodName;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set methodName
+     *
+     * @param string $methodName
+     *
+     * @return PaymentMethod
+     */
+    public function setMethodName($methodName)
+    {
+        $this->methodName = $methodName;
+
+        return $this;
+    }
+
+    /**
+     * Get methodName
+     *
+     * @return string 
+     */
+    public function getMethodName()
+    {
+        return $this->methodName;
+    }
+}
+
