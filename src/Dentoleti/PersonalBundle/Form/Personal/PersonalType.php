3d44d4df849cefc594580f1b4e07336e44bf6daa
luismagr <desarrollo@connect2tic.es>
2014-04-05 09:58:07 +0200

    Issue #167. Añadido el autocompletado a falta de corregir error al embeber el formulario de Personal

--- src/Dentoleti/PersonalBundle/Form/Personal/PersonalType.php
+++ src/Dentoleti/PersonalBundle/Form/Personal/PersonalType.php
@@ -4,6 +4,10 @@ namespace Dentoleti\PersonalBundle\Form\Personal;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Dentoleti\GeneralBundle\Entity\PostalCode;
 
 class PersonalType extends AbstractType
 {
@@ -21,12 +25,41 @@ class PersonalType extends AbstractType
 			'empty_value' => '-- Código postal --',
 			'required' => false
 		));
-		$builder->add('town', 'entity', array(
-			'class' => 'DentoletiGeneralBundle:Town',
-			'property' => 'name',
-			'empty_value' => '-- POBLACIÓN --',
-			'required' => false
-		));
+
+		$formModifier = function(FormInterface $form, PostalCode $postalCode = null) {
+			$towns = null === $postalCode ? array() : $postalCode->getTowns();
+
+			$form->add('town', 'entity', array(
+				'class' => 'DentoletiGeneralBundle:Town',
+				'property' => 'name',
+				'empty_value' => '-- POBLACIÓN --',
+				'required' => false,
+				'choices' => $towns
+			));
+		};
+
+		$builder->addEventListener(
+			FormEvents::PRE_SET_DATA,
+			function(FormEvent $event) use ($formModifier) {
+                // this would be my entity
+                $data = $event->getData();
+                
+                $formModifier($event->getForm(), $data->getPostalCode());
+            }
+		);
+
+		$builder->get('postalCode')->addEventListener(
+			FormEvents::POST_SUBMIT,
+            function(FormEvent $event) use ($formModifier) {
+                // It's important here to fetch $event->getForm()->getData(), as
+                // $event->getData() will get you the client data (that is, the ID)
+                $postalCode = $event->getForm()->getData();
+
+                // since we've added the listener to the child, we'll have to pass on
+                // the parent to the callback functions!
+                $formModifier($event->getForm()->getParent(), $postalCode);
+            }
+		);
 		$builder->add('phone1', 'text', array(
 			'required' => false
 		));
