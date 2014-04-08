d67d235cdf5ae5e8ac9e3438fd5448b61f92175c
luismagr <desarrollo@connect2tic.es>
2014-04-08 09:47:16 +0200

    Issue #187. Edición y listado de cita

--- src/Dentoleti/ConsultationBundle/Controller/DefaultController.php
+++ src/Dentoleti/ConsultationBundle/Controller/DefaultController.php
@@ -8,34 +8,75 @@ use Dentoleti\ConsultationBundle\Form\Consultation\ConsultationType;
 
 class DefaultController extends Controller
 {
-   public function addAction()
-   {
-   		$petition = $this->getRequest();
+  public function addAction()
+  {
+    $petition = $this->getRequest();
 
-    	$consultation = new Consultation();
-		
-		$form = $this->createForm(new ConsultationType(), $consultation);
-		
-		$consultation->setConcertationDate(new \DateTime());
+  	$consultation = new Consultation();
+	
+	  $form = $this->createForm(new ConsultationType(), $consultation);
+	
+	  $consultation->setConcertationDate(new \DateTime());
 
-		$form->handleRequest($petition);
+	  $form->handleRequest($petition);
 
-		if ($form->isValid()){
-            //save the form
-    		$em = $this->getDoctrine()->getManager();
-    		
-    		$em->persist($consultation);
-    		$em->flush();
+	  if ($form->isValid()){
+  		$em = $this->getDoctrine()->getManager();
+  		
+  		$em->persist($consultation);
+  		$em->flush();
 
-            $this->get('session')->getFlashBag()->add(
-              'notice',
-              'La cita se ha guardado correctamente'
-            );
+      $this->get('session')->getFlashBag()->add(
+        'notice',
+        'La cita se ha guardado correctamente'
+      );
 
-     	}
+   	}
 
-        return $this->render('DentoletiConsultationBundle:Default:consultation.html.twig', array(
-        	'form' => $form->createView()
-        ));
+    return $this->render('DentoletiConsultationBundle:Default:consultation.html.twig', array(
+    	'form' => $form->createView()
+    ));
    }
+
+  public function editAction($id)
+  {
+    $petition = $this->getRequest();
+
+    $em = $this->getDoctrine()->getManager();
+
+    $consultation = $em->getRepository('DentoletiConsultationBundle:Consultation')
+        ->findOneById($id);
+
+    if (!$consultation) {
+        throw $this->createNotFoundException('No existe la cita');
+    }
+
+    $form = $this->createForm(new ConsultationType(), $consultation);
+    
+    $form->handleRequest($petition);
+
+    $em->persist($consultation);
+    $em->flush();
+
+    $this->get('session')->getFlashBag()->add(
+        'notice',
+        'La cita se ha actualizado correctamente'
+      );
+    
+    return $this->render('DentoletiConsultationBundle:Default:consultation.html.twig', array(
+        'form' => $form->createView()
+    ));
+  }
+
+  public function listAction()
+  {
+    $em = $this->getDoctrine()->getManager();
+
+    $consultationList = $em->getRepository('DentoletiConsultationBundle:Consultation')
+      ->findAll();
+
+    return $this->render('DentoletiConsultationBundle:Default:list.html.twig', array(
+      'consultationList' => $consultationList
+    ));
+  }
 }
