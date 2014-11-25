d98c881d64186dbfe31bca4ae2ef33c92d30757a
luismagr <desarrollo@luismagonzalez.es>
2014-11-25 07:07:03 +0100

    Reindent code

--- src/Dentoleti/TreatmentBundle/Controller/DefaultController.php
+++ src/Dentoleti/TreatmentBundle/Controller/DefaultController.php
@@ -38,215 +38,210 @@ use Dentoleti\AccountingBundle\Entity\Debt;
 use Dentoleti\TreatmentBundle\Form\Treatment\TreatmentType;
 use Dentoleti\TreatmentBundle\Helper\TreatmentUtils;
 
-class DefaultController extends Controller
-{
-    /**
-     * Add a new treatment in the system
-     */
-    public function addAction($budgetId)
-    {
-      $petition = $this->container->get('request_stack')->getCurrentRequest();
-      $treatment = new Treatment();
-      $debt = new Debt();
+class DefaultController extends Controller {
+  /**
+   * Add a new treatment in the system
+   */
+  public function addAction($budgetId) {
+    $petition = $this->container->get('request_stack')->getCurrentRequest();
+    $treatment = new Treatment();
+    $debt = new Debt();
 
-      $form = $this->createForm(new TreatmentType(), $treatment);
-		
-		  $treatment->setTreatmentDate(new \DateTime());
+    $form = $this->createForm(new TreatmentType(), $treatment);
 
-		  $form->handleRequest($petition);
+    $treatment->setTreatmentDate(new \DateTime());
 
-		  if ($form->isValid()){
-        // Get the entity manager
-        $em = $this->getDoctrine()->getManager();
+    $form->handleRequest($petition);
 
-        $budget = $em->getRepository('DentoletiBudgetBundle:Budget')
-          ->findOneById($budgetId);
-        $budget->setConfirmed(true);
-        $em->persist($budget);
-
-        $treatment->setBudget($budget);
-        $em->persist($treatment);
-
-        $budgetDetailsList = $em->getRepository('DentoletiBudgetBundle:BudgetDetail')
-          ->findArticlesOfBudget($budget);
-
-        $partialTotals = array();
-        $subTotals = array();
-        $ivas = array();
-
-        $total = 0;
-        foreach ($budgetDetailsList as $budgetDetail) {
-          $partial = $budgetDetail->getAmount() * $budgetDetail->getPrice();
-          $ivas[$budgetDetail->getId()] = 
-              $budgetDetail->getArticle()->getVat() * $partial;
-          
-          $partialTotals[$budgetDetail->getId()] = $partial;
-          $subTotals[$budgetDetail->getId()] =
-              $partial + $ivas[$budgetDetail->getId()];
-          $total = $total + $subTotals[$budgetDetail->getId()];
-          
-        }
-        $debt->setTreatment($treatment);
-        $debt->setAmount($total);
-
-        //save the form
-    		$em->persist($treatment);
-        //save the debt
-        $em->persist($debt);
-    		$em->flush();
-
-        $this->get('session')->getFlashBag()->add(
-          'notice',
-          'El tratamiento se ha guardado correctamente'
-        );
-
-        $nextAction = $form->get('save')->isClicked()
-          ? 'treatment_view' //TODO cambiar por treatment_details_add
-          : 'treatment_add';
-
-        if ('treatment_view' == $nextAction){
-          return $this->redirect($this->generateUrl($nextAction, array(
-            'id' => $treatment->getId())));
-        }
-
-        return $this->redirect($this->generateUrl($nextAction));
-     	}
-
-      return $this->render('DentoletiTreatmentBundle:Default:treatment.html.twig', array(
-       	'form' => $form->createView()
-      ));
-    }
-
-    /**
-     * List all the treatments in the system
-     */
-    public function listAction()
-    {
+    if ($form->isValid()) {
+      // Get the entity manager
       $em = $this->getDoctrine()->getManager();
 
-      $treatmentList = $em->getRepository('DentoletiTreatmentBundle:Treatment')
-        ->findAll();
+      $budget = $em->getRepository('DentoletiBudgetBundle:Budget')
+        ->findOneById($budgetId);
+      $budget->setConfirmed(TRUE);
+      $em->persist($budget);
 
-      return $this->render('DentoletiTreatmentBundle:Default:list.html.twig', array(
-        'treatmentList' => $treatmentList
-      ));
+      $treatment->setBudget($budget);
+      $em->persist($treatment);
+
+      $budgetDetailsList = $em->getRepository('DentoletiBudgetBundle:BudgetDetail')
+        ->findArticlesOfBudget($budget);
+
+      $partialTotals = array();
+      $subTotals = array();
+      $ivas = array();
+
+      $total = 0;
+      foreach ($budgetDetailsList as $budgetDetail) {
+        $partial = $budgetDetail->getAmount() * $budgetDetail->getPrice();
+        $ivas[$budgetDetail->getId()] =
+          $budgetDetail->getArticle()->getVat() * $partial;
+
+        $partialTotals[$budgetDetail->getId()] = $partial;
+        $subTotals[$budgetDetail->getId()] =
+          $partial + $ivas[$budgetDetail->getId()];
+        $total = $total + $subTotals[$budgetDetail->getId()];
+
+      }
+      $debt->setTreatment($treatment);
+      $debt->setAmount($total);
+
+      //save the form
+      $em->persist($treatment);
+      //save the debt
+      $em->persist($debt);
+      $em->flush();
+
+      $this->get('session')->getFlashBag()->add(
+        'notice',
+        'El tratamiento se ha guardado correctamente'
+      );
+
+      $nextAction = $form->get('save')->isClicked()
+        ? 'treatment_view' //TODO cambiar por treatment_details_add
+        : 'treatment_add';
+
+      if ('treatment_view' == $nextAction) {
+        return $this->redirect($this->generateUrl($nextAction, array(
+          'id' => $treatment->getId()
+        )));
+      }
+
+      return $this->redirect($this->generateUrl($nextAction));
     }
 
-    /**
-     * Method for view all the treatment's information
-     */
-    public function viewAction($id)
-    {
-        $log = $this->get('monolog.logger.dentoleti');
-        $log->info("Vista del usuario " . $id);
-        
-        $em = $this->getDoctrine()->getManager();
+    return $this->render('DentoletiTreatmentBundle:Default:treatment.html.twig', array(
+      'form' => $form->createView()
+    ));
+  }
 
-        $treatment = $em->getRepository('DentoletiTreatmentBundle:Treatment')
-            ->findOneById($id);
+  /**
+   * List all the treatments in the system
+   */
+  public function listAction() {
+    $em = $this->getDoctrine()->getManager();
 
-        $budget_details = $em->getRepository('DentoletiBudgetBundle:BudgetDetail')
-          ->findBudgetDetails($treatment->getBudget());
+    $treatmentList = $em->getRepository('DentoletiTreatmentBundle:Treatment')
+      ->findAll();
 
-        $posting_lines = $em->getRepository('DentoletiAccountingBundle:PostingLine')
-          ->findPostingLineForTreatment($id);
+    return $this->render('DentoletiTreatmentBundle:Default:list.html.twig', array(
+      'treatmentList' => $treatmentList
+    ));
+  }
 
-        $articles = array();
-        foreach ($budget_details as $budget_detail) {
-          $articles[] = $budget_detail->getArticle();
-        }
+  /**
+   * Method for view all the treatment's information
+   */
+  public function viewAction($id) {
+    $log = $this->get('monolog.logger.dentoleti');
+    $log->info("Vista del usuario " . $id);
 
-        if (!$treatment) {
-            throw $this->createNotFoundException('No existe el tratamiento');
-        }
+    $em = $this->getDoctrine()->getManager();
 
-        $partialTotals = array();
-        $subTotals = array();
-        $ivas = array();
+    $treatment = $em->getRepository('DentoletiTreatmentBundle:Treatment')
+      ->findOneById($id);
 
-        $total = 0;
-        foreach ($budget_details as $budgetDetail) {
-            $partial = $budgetDetail->getAmount() * $budgetDetail->getPrice();
-            $ivas[$budgetDetail->getId()] = 
-                $budgetDetail->getArticle()->getVat() * $partial;
+    $budget_details = $em->getRepository('DentoletiBudgetBundle:BudgetDetail')
+      ->findBudgetDetails($treatment->getBudget());
 
-            $partialTotals[$budgetDetail->getId()] = $partial;
-            $subTotals[$budgetDetail->getId()] =
-                $partial + $ivas[$budgetDetail->getId()];
-            $total = $total + $subTotals[$budgetDetail->getId()];
-        }
+    $posting_lines = $em->getRepository('DentoletiAccountingBundle:PostingLine')
+      ->findPostingLineForTreatment($id);
 
-        $paid = 0;
-        foreach ($posting_lines as $pl) {
-          $paid = $pl->getAmount();
-        }
-
-        return $this->render('DentoletiTreatmentBundle:Default:treatment_view.html.twig', array(
-            'treatment' => $treatment,
-            'articles' => $articles,
-            'postingLines' => $posting_lines,
-            'total' => $total,
-            'paid' => $paid
-        ));
+    $articles = array();
+    foreach ($budget_details as $budget_detail) {
+      $articles[] = $budget_detail->getArticle();
     }
 
-    /**
-     * Edit the treatment with the $id given in the params
-     */
-    public function editAction($id)
-    {
-      $petition = $this->container->get('request_stack')->getCurrentRequest();
-
-        $em = $this->getDoctrine()->getManager();
-
-        $treatment = $em->getRepository('DentoletiTreatmentBundle:Treatment')
-            ->findOneById($id);
-
-        if (!$treatment) {
-            throw $this->createNotFoundException('No existe el tratamiento');
-        }
-
-        $form = $this->createForm(new TreatmentType(), $treatment);
-        
-        $form->handleRequest($petition);
-
-        $em->persist($treatment);
-        $em->flush();
-        
-        return $this->render('DentoletiTreatmentBundle:Default:treatment.html.twig', array(
-            'form' => $form->createView()
-        ));
+    if (!$treatment) {
+      throw $this->createNotFoundException('No existe el tratamiento');
     }
 
-    /**
-     * ATTENTION
-     *
-     * Delete method for deleting one personal given by the id.
-     * This will delete the record and all the relations with other entities
-     * so that, USE IT WITH CAREFULL
-     */
-    public function deleteAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
+    $partialTotals = array();
+    $subTotals = array();
+    $ivas = array();
 
-        $treatment = $em->getRepository('DentoletiTreatmentBundle:Treatment')
-            ->findOneById($id);
+    $total = 0;
+    foreach ($budget_details as $budgetDetail) {
+      $partial = $budgetDetail->getAmount() * $budgetDetail->getPrice();
+      $ivas[$budgetDetail->getId()] =
+        $budgetDetail->getArticle()->getVat() * $partial;
 
-        $treatmentUtils = new TreatmentUtils();
-        $treatment = $treatmentUtils->cancelTreatment($treatment);
-
-        $dentoletiDebtService = $this->get('dentoleti_debts');
-        $dentoletiDebtService->createDebtCancelled($treatment->getId());
-
-        if (!$treatment) {
-            throw $this->createNotFoundException('No existe el tratamiento');
-        }
-        else {
-            $em->persist($treatment);
-            $em->flush();
-        }
-
-        //TODO Pendiente de ver donde redirigir la petición
-        return $this->forward('DentoletiTreatmentBundle:Default:list');
+      $partialTotals[$budgetDetail->getId()] = $partial;
+      $subTotals[$budgetDetail->getId()] =
+        $partial + $ivas[$budgetDetail->getId()];
+      $total = $total + $subTotals[$budgetDetail->getId()];
     }
+
+    $paid = 0;
+    foreach ($posting_lines as $pl) {
+      $paid = $pl->getAmount();
+    }
+
+    return $this->render('DentoletiTreatmentBundle:Default:treatment_view.html.twig', array(
+      'treatment' => $treatment,
+      'articles' => $articles,
+      'postingLines' => $posting_lines,
+      'total' => $total,
+      'paid' => $paid
+    ));
+  }
+
+  /**
+   * Edit the treatment with the $id given in the params
+   */
+  public function editAction($id) {
+    $petition = $this->container->get('request_stack')->getCurrentRequest();
+
+    $em = $this->getDoctrine()->getManager();
+
+    $treatment = $em->getRepository('DentoletiTreatmentBundle:Treatment')
+      ->findOneById($id);
+
+    if (!$treatment) {
+      throw $this->createNotFoundException('No existe el tratamiento');
+    }
+
+    $form = $this->createForm(new TreatmentType(), $treatment);
+
+    $form->handleRequest($petition);
+
+    $em->persist($treatment);
+    $em->flush();
+
+    return $this->render('DentoletiTreatmentBundle:Default:treatment.html.twig', array(
+      'form' => $form->createView()
+    ));
+  }
+
+  /**
+   * ATTENTION
+   *
+   * Delete method for deleting one personal given by the id.
+   * This will delete the record and all the relations with other entities
+   * so that, USE IT WITH CAREFULL
+   */
+  public function deleteAction($id) {
+    $em = $this->getDoctrine()->getManager();
+
+    $treatment = $em->getRepository('DentoletiTreatmentBundle:Treatment')
+      ->findOneById($id);
+
+    $treatmentUtils = new TreatmentUtils();
+    $treatment = $treatmentUtils->cancelTreatment($treatment);
+
+    $dentoletiDebtService = $this->get('dentoleti_debts');
+    $dentoletiDebtService->createDebtCancelled($treatment->getId());
+
+    if (!$treatment) {
+      throw $this->createNotFoundException('No existe el tratamiento');
+    }
+    else {
+      $em->persist($treatment);
+      $em->flush();
+    }
+
+    //TODO Pendiente de ver donde redirigir la petición
+    return $this->forward('DentoletiTreatmentBundle:Default:list');
+  }
 }
